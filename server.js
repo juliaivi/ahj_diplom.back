a5ca05ebcec0d95777b8b694a00d0e6533e9161c
juliaivi <juliashinobi@mail.ru>
2023-10-06 09:01:17 +0300

    change server

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// import * as http from 'http';
 const http = require('http');
 const path = require('path');
 const Koa = require('koa');
@@ -7,28 +6,27 @@ const cors = require('@koa/cors');
 const Router = require('koa-router');;
 const { faker } = require('@faker-js/faker');
 const koaStatic = require('koa-static');
-const fs = require('fs');// для работы с файлами
-// import WebSocket, { WebSocketServer } from "ws"; // для общения сервака и клиента.Протокол связи поверх TCP-соединения, предназначенный для обмена сообщениями между браузером и веб-сервером, используя постоянное соединение. 
+const fs = require('fs');
 const WS = require('ws');
 const { v4: uuidv4 } = require('uuid');
 const WebSocketServer = require('ws').Server;
 
 const app = new Koa();
-const router = new Router(); // создание роутера
-const pub = path.join(__dirname, '/public'); // // итоговая папка которую раздаем назначить определенную папку, как папку которую мы будем раздавать по http. Раздаваеть ее содержимое как статические файлы
+const router = new Router();
+const pub = path.join(__dirname, '/public');
 app.use(koaStatic(pub));
 
 app.use(cors({
     origin: '*',
-    credentials: true, //Если сервер согласен принять запрос с авторизационными данными, он должен добавить заголовок Access-Control-Allow-Credentials: true к ответу, в дополнение к Access-Control-Allow-Origin.
+    credentials: true,
     'Access-Control-Allow-Origin': true,
     allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 )
 
 app.use(koaBody({
-   urlencoded: true, // форма у нас url  ин kодед. Данные будут закодированы. Объект со всеми разобранными данными
-   multipart: true,// при использовании multipart/form-data
+   urlencoded: true,
+   multipart: true,
    json: true, 
   }));
 
@@ -36,58 +34,49 @@ const allMessages = [];
 const userState = []; 
 let userName = null;
 
-  // ВХОД - ЗАПРОС
   router.post("/new-user", async (ctx) => {
-    if (Object.keys(ctx.request.body).length === 0) {// Object.keys() возвращает массив из собственных перечисляемых свойств переданного объекта. Если будет пустой
+    if (Object.keys(ctx.request.body).length === 0) {
         const result = {
-        status: "error",// ошибка
-        message: "This name is already taken!", // Это имя уже занято!
+        status: "error",
+        message: "This name is already taken!",
       };
       ctx.response.body = result;
     }
-    const { name } = ctx.request.body; // с тела запроса берем имя
-    const isExist = userState.find((user) => user.name === name); // сравниваем с текущими именами
+    const { name } = ctx.request.body;
+    const isExist = userState.find((user) => user.name === name);
     if (!isExist) { 
-      userName = name; // если не находит создает новый объект с таким именем и присваевает айдишник
+      userName = name;
       const newUser = {
         id: uuidv4(),
         name: name,
       };
-      userState.push(newUser);// добавляет к списку имен
-      const result = { // положительный статус который вернет он фронту с новым именем
+      userState.push(newUser);
+      const result = {
         status: "ok",
         user: newUser,
       };
       ctx.response.body = result;
-    } else {// вернет ошибку
+    } else {
       const result = {
         status: "error",
-        message: "This name is already taken!",// Это имя уже занято
+        message: "This name is already taken!",
       };
       ctx.response.body = result;
     }
   });
 
-app.use(router.routes()).use(router.allowedMethods()) // важно отправить все сформированные роутеры в app.use
-//routes этот метод работает также как мидл вейр позволяет внедрить синтаксис router в app.use в Koa
-//////////////////////////////////////////////////////////
+app.use(router.routes()).use(router.allowedMethods())
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app.callback());
-// паралельная сущность
-const wsServer = new WebSocketServer({ server }); // в него передаем текущий настроенный http сервер. Это сущность которая существует паралельно с основным http сервером и использует подключение для своей работ
-// теперь сервер для вебсокетов встроился в наш текущий флоу работы
+const wsServer = new WebSocketServer({ server });
 
 let users = [];
-wsServer.on("connection", (ws) => { // прослушка соеденения (подписывание на события). Объект с поощью которого можно рассылать сообщения
+wsServer.on("connection", (ws) => {
   users.push({userName , ws})
-  console.log(users)
 
-
-  ws.on("message", (msg, isBinary) => {// прослушка сообщений
-    console.log(1)
-    const receivedMSG = JSON.parse(msg); // получить сообщение . Преобразовывает в объект JSON.parse() Статический метод анализирует строку JSON, создавая значение JavaScript или объект, описываемый строкой
-    console.dir(receivedMSG); //это способ посмотреть в консоли свойства заданного javascript объекта
+  ws.on("message", (msg, isBinary) => {
+    const receivedMSG = JSON.parse(msg); 
     const obj = {
       message: receivedMSG,
       dataId: receivedMSG.dataId,
@@ -95,28 +84,23 @@ wsServer.on("connection", (ws) => { // прослушка соеденения (
 
     allMessages.push(obj);
 
-    //ВЫХОД УЧАСТНИКА
-    if (receivedMSG.type === "exit") { // выход, если кто-то хочет ути с чата(отключится?)
-      console.log(2);
-      const idx = userState.findIndex( // находим такое имя в списке
+    if (receivedMSG.type === "exit") {
+      const idx = userState.findIndex(
         (user) => user.name === receivedMSG.name);
-      userState.splice(idx, 1); // удоляем его из списка
-      [...wsServer.clients] // wsServer.clients
-        .filter((o) => o.readyState === WS.OPEN) // фильтруем масив и оставляем только тех кто открыт (есть онлайн)
-        .forEach((o) => o.send(JSON.stringify(userState))); // // отправляем каждому список оставшихся собеседников
+      userState.splice(idx, 1);
+      [...wsServer.clients]
+        .filter((o) => o.readyState === WS.OPEN) 
+        .forEach((o) => o.send(JSON.stringify(userState)));
       return;
     }
     
-    //ОТПРАВКА СООБЩЕНИЙ
-    if (receivedMSG.type === "send") { // отправить
-      console.log(3);
-      [...wsServer.clients] // массив со всеми участниками
-        .filter((o) => o.readyState === WS.OPEN) // смотрим чтоб все были в сети
-        .forEach((o) => o.send(msg, { binary: isBinary })); // отправляем каждому сообщение, чтоб отображалось. Что это за метод{ binary: isBinary }?
+    if (receivedMSG.type === "send") { 
+      [...wsServer.clients]
+        .filter((o) => o.readyState === WS.OPEN)
+        .forEach((o) => o.send(msg, { binary: isBinary }));
     }
 
-        //ОТПРАВКА СООБЩЕНИЙ
-    if (receivedMSG.type === "sendAll") { // отправить
+    if (receivedMSG.type === "sendAll") {
       let indexElem = allMessages.map((el) => el.dataId).indexOf(Number(receivedMSG.dataId));
       let listMessege = allMessages.slice(0, indexElem);
       let userPosition = users.map((el) => el.userName).indexOf(receivedMSG.name);
@@ -125,15 +109,100 @@ wsServer.on("connection", (ws) => { // прослушка соеденения (
         message = listMessege.slice(-10);
       }
       let newMessageBlok = message.reverse();
-      console.log({type: 'allsms', message: message})
       users[userPosition].ws.send(JSON.stringify({type: 'allsms', message: newMessageBlok}));
       }    
   });
 
-  [...wsServer.clients]// массив со всеми участниками
-    .filter((o) => o.readyState === WS.OPEN) // отфильтровывает и оставляет тех кто в сети
-    .forEach((o) => o.send(JSON.stringify(userState))); // отправляем каждому список оставшихся собеседников
+  [...wsServer.clients]
+    .filter((o) => o.readyState === WS.OPEN)
+    .forEach((o) => o.send(JSON.stringify(userState))); 
 });
 
 server.listen(port);
-console.log(`The server started on port ${port}`); 
+
+
+
+
+
+
+
+
+
+// const http = require('http');
+// const path = require('path');
+// const Koa = require('koa');
+// const koaBody = require('koa-body').default;
+// const cors = require('@koa/cors');
+// const Router = require('koa-router');
+// const { faker } = require('@faker-js/faker');
+// const koaStatic = require('koa-static');
+// const fs = require('fs');
+// const { v4: uuidv4 } = require('uuid');
+// const app = new Koa();
+// const router = new Router(); 
+// const pub = path.join(__dirname, '/public'); 
+// app.use(koaStatic(pub));
+// const WebSocket = require('./WS');
+
+// app.use(cors({
+//     origin: '*',
+//     credentials: true,
+//     'Access-Control-Allow-Origin': true,
+//     allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//   })
+// )
+
+// app.use(koaBody({
+//    urlencoded: true,
+//    multipart: true,
+//    json: true, 
+//   }));
+
+// const userState = []; 
+// let userName = null;
+
+//   router.post("/new-user", async (ctx) => {
+//     if (Object.keys(ctx.request.body).length === 0) {
+//         const result = {
+//         status: "error",
+//         message: "This name is already taken!",
+//       };
+//       ctx.response.body = result;
+//     }
+//     const { name } = ctx.request.body;
+//     const isExist = userState.find((user) => user.name === name);
+//     if (!isExist) { 
+//       userName = name;
+//       const newUser = {
+//         id: uuidv4(),
+//         name: name,
+//       };
+//       userState.push(newUser);
+//       const result = {
+//         status: "ok",
+//         user: newUser,
+//       };
+// // WebSocket.WebSocket({ server }, userState, userName);
+//       ctx.response.body = result;
+//     } else {
+//       const result = {
+//         status: "error",
+//         message: "This name is already taken!",
+//       };
+//       ctx.response.body = result;
+//     }
+//   });
+
+
+
+// app.use(router.routes()).use(router.allowedMethods()) 
+
+// const port = process.env.PORT || 3000;
+// const server = http.createServer(app.callback());
+// console.log('userName', userName)
+// console.log('userState', userState)
+
+//   WebSocket.WebSocket({ server }, userState, userName);
+
+
+// server.listen(port); 
